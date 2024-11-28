88105ff24c4a7069ca547bcc7c39965c5265caaf
marieemoiselle <jcavila700@gmail.com>
2024-11-28 17:18:37 +0800

    commit

--- lib/screens/location_page.dart
+++ lib/screens/location_page.dart
@@ -3,7 +3,32 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 
-class LocationPage extends StatelessWidget {
+class LocationPage extends StatefulWidget {
+  @override
+  _LocationPageState createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  // Default coordinates (initial position of the map)
+  LatLng _center = LatLng(13.7852, 121.0748);
+  double _zoom = 13.0;
+
+  // Function to simulate updating GPS coordinates
+  void _updateLocation(double lat, double lon) {
+    setState(() {
+      _center = LatLng(lat, lon); // Update the center position to GPS coordinates
+    });
+  }
+
+  // Function to simulate receiving GPS data
+  void _refreshMap() {
+    // Simulating new GPS data
+    double newLat = 13.7612; // New latitude (could be from GPS)
+    double newLon = 121.0588; // New longitude (could be from GPS)
+    
+    _updateLocation(newLat, newLon);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +42,8 @@ class LocationPage extends StatelessWidget {
       ),
       body: FlutterMap(
         options: MapOptions(
-          initialCenter: LatLng(13.7852, 121.0748), // Centered over BSU Alangilan
-          initialZoom: 13.0, // Set the initial zoom level
+          initialCenter: _center,  // Center the map based on the current coordinates
+          initialZoom: _zoom,      // Set zoom level
         ),
         children: [
           TileLayer(
@@ -31,7 +56,7 @@ class LocationPage extends StatelessWidget {
               Marker(
                 width: 80.0,
                 height: 80.0,
-                point: LatLng(13.7612, 121.0588), // Coordinates for BSU Alangilan
+                point: _center, // Place marker on the current center position
                 child: Icon(
                   Icons.location_pin,
                   color: Colors.red,
@@ -41,6 +66,11 @@ class LocationPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _refreshMap, // Calls the refresh function when pressed
+        child: Icon(Icons.refresh), // Refresh icon
+        backgroundColor: Colors.blue,
       ),
     );
   }
